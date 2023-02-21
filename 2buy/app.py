71f4e64911a21248b16a8e3650b982e204783c82
Marc Chester Masakayan <86514651+mcdmasakayan@users.noreply.github.com>
2023-02-21 13:47:18 +0800

    update

--- 2buy/app.py
+++ 2buy/app.py
@@ -1,82 +1,85 @@
 from flask import Flask, render_template, redirect, url_for, request
 import mysql.connector as mysql
 
+class States:
+    logged_in = False
+    connected = False
+
 app = Flask(__name__)
+state = States()
 
-app.secret_key = "root123"
-logged_in = False
+#Database
+while(state.connected == False):
+        try:    #if database exists#
+            db = mysql.connect(host="localhost", port=3306, user="root", passwd="root", database="2Buy")
+            crsr = db.cursor()
+            
+            try:    #if table does not exist#
+                query = """CREATE TABLE items (id int PRIMARY KEY AUTO_INCREMENT, quantity int UNSIGNED,
+                        name VARCHAR(255), type VARCHAR(255), price int UNSIGNED)"""
 
-#Database#
-connected = False
+                crsr.execute(query)
 
-while(connected == False):
-    try:    #if database exists#
-        global db, crsr
+            except:    #if table exists#
+                print("SYSTEM: Database connected.")
+                state.connected = True
+                crsr.execute("SELECT * FROM items")
+                data = crsr.fetchall()
 
-        db = mysql.connect(host="localhost", port=3306, user="root", passwd="root", database="2Buy")
-        crsr = db.cursor()
-        
-        try:    #if table does not exist#
-            query = """CREATE TABLE items (id int PRIMARY KEY AUTO_INCREMENT, quantity int UNSIGNED,
-                    name VARCHAR(255), type VARCHAR(255), price int UNSIGNED)"""
+        except:    #if database does not exist#
+            db = mysql.connect(host="localhost", port=3306, user="root", passwd="root")
+            crsr = db.cursor()
+            crsr.execute("CREATE DATABASE 2Buy")
 
-            crsr.execute(query)
-
-        except:    #if table exists#
-            connected = True
-
-    except:    #if database does not exist#
-        db = mysql.connect(host="localhost", port=3306, user="root", passwd="root")
-        crsr = db.cursor()
-        crsr.execute("CREATE DATABASE 2Buy")
-
-#Data Handling#
-def insert_data(item_quantity, item_name, item_type, item_price):
-    crsr.execute("INSERT INTO items (quantity, name, type, price) VALUES (%s, %s, %s, %s)",
-                     (item_quantity, item_name, item_type, item_price))
-    db.commit()
-
-#Landing Page#
-@app.route("/")
-def base_page():
-    if logged_in == False:
-        return redirect(url_for("login_page"))
+#Web#
+@app.route("/", methods=["POST", "GET"])
+def base():
+    if state.logged_in == True:
+        print(data)
+        return redirect(url_for("home_page", user="Backend", login_state=state.logged_in, data=data))
     else:
-        return redirect(url_for("home_page"))
+        return redirect(url_for("login_page"))
 
-@app.route("/login")
+@app.route("/2buy-login", methods = ["POST", "GET"])
 def login_page():
-    page = "Login"
-
-    return render_template("login.html", page_title=page)
-
-@app.route("/signup")
-def signup_page():
-    page = "Signup"
-
-    return render_template("signup.html", page_title=page)
-
-#Home Page#
-@app.route("/home", methods = ["POST", "GET"])
-def home_page():
-    page = "Home"
+    page_name = "Login"
+    state.logged_in = False
 
     if request.method == "POST":
-        item_quantity = request.form['quantity-entry']
-        item_name = request.form['name-entry']
-        item_type = request.form['type-entry']
-        item_price = request.form['price-entry']
+        if (request.form['username-entry'] == "backend") and (request.form['password-entry'] == "developer"):
+            state.logged_in = True
 
-        insert_data(item_quantity, item_name,
-                    item_type, item_price)
-        
-        return redirect(url_for("home_page", page_title=page,
-                                item_quantity=item_quantity,
-                                item_name=item_name,
-                                item_type=item_type,
-                                item_price=item_price))
+    if state.logged_in == False:
+        state.logged_in = False
+       
+        return render_template("login.html", page_title=page_name)
+    
     else:
-        return render_template("home.html", page_title=page)
+        state.logged_in = True
+        return redirect(url_for("base"))
+
+@app.route("/2buy-<user>", methods=["POST", "GET"])
+def home_page(user):
+    page_name = user
+
+    if request.method == "POST" and (request.form['quantity-entry'] and request.form['name-entry']
+                                     and request.form['type-entry'] and request.form['price-entry']):
+        crsr.execute("INSERT INTO items (quantity, name, type, price) VALUES (%s, %s, %s, %s)",
+                     (request.form['quantity-entry'], request.form['name-entry'],
+                      request.form['type-entry'], request.form['price-entry']))
+        db.commit()
+
+    crsr.execute("SELECT * FROM items")
+    data = crsr.fetchall()
+        
+    if state.logged_in == True:
+        state.logged_in = True
+
+        return render_template("home.html", page_title=page_name, login_state=state.logged_in, data=data)
+    else:
+        state.logged_in = False
+        return redirect(url_for("login_page"))
 
 if __name__ == "__main__":
+    app.secret_key = "2buykey"
     app.run(debug=True)
--- 2buy/static/scripts/navigation.js
+++ 2buy/static/scripts/navigation.js
@@ -1,21 +1,9 @@
+/*Item Entry*/
 const table = document.getElementById("list-table");
 const addBox = document.getElementById("add-box");
 const addButton = document.getElementById("add-item")
 const submitButton = document.getElementById("submit-item");
 const closeButton = document.getElementsByClassName("close-button")[0];
-
-function validate(){
-    let loginForm = document.getElementById("login-form");
-    let username = document.getElementById("username-entry");
-    let password = document.getElementById("password-entry");
-
-    if(username.value == "backend" && password.value  == "developer"){
-        loginForm.action = "/home";
-    }
-    else{
-        loginForm.action = "#";
-    }
-}
 
 addButton.onclick = function(){
     addBox.style.display = "block";
@@ -29,4 +17,14 @@ window.onclick = function(event){
     if(event.target == addBox){
         addBox.style.display = "none";
     }
+}
+
+/*Logout*/
+const userPage = document.getElementsByClassName("user-page");
+const loginState = document.getElementById("login-state");
+const logoutButton = document.getElementById("logout-button");
+
+logoutButton.onclick = function(){
+    loginState.value = "0";
+    userPage.action = "/2buy-<user>";
 }
